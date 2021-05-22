4c90be1095db1368d3ce7d40c5d21f7b0c7c1b16
Surf-house <reeeee@ukr.net>
2021-05-22 15:19:10 +0300

    Method clearupParameter can accept parameters in url

--- public_html/app/Core/Application.controller.php
+++ public_html/app/Core/Application.controller.php
@@ -8,9 +8,13 @@ class Application
     static protected $prams = [];
 
 
+    // the method bellow calls to prepared
     static public function call_by_url()
     {
+      try {
         self::prepareCALL();
+
+        // the code below for name of controller in lower case
         if(file_exists(CONTROLLER. self::$controller . '.controller.php'))
         {
             self::$controller = new self::$controller;
@@ -18,11 +22,28 @@ class Application
                  call_user_func_array([self::$controller, self::$action], self::$prams);
             }
         }
+        // the code below for name of controller in upper case
+        elseif (file_exists(CONTROLLER. ucfirst(self::$controller) . '.controller.php'))
+        {
+            self::$controller = new self::$controller;
+            if(method_exists(self::$controller, self::$action)) {
+                 call_user_func_array([self::$controller, self::$action], self::$prams);
+            }
+        }
+        else {
+          throw new Exception("Method call_by_url wasn't successful, file of current controller haven't found");
+             }
+           } catch (Exception $exception) {
+            file_put_contents("my-errors.log", 'Message:' . $exception->getMessage() . '<br />'.   'File: ' . $exception->getFile() . '<br />' .
+            'Line: ' . $exception->getLine() . '<br />' .'Trace: ' . $exception->getTraceAsString());
+                                         }
+        
     }
 
     // the  method below  prepares url for call  
     static protected function prepareCALL()
     {
+        
             $url = self::clearupParameter();
             // the code below sets controller name for calling
             self::$controller = isset($url[0]) ? $url[0].'Controller' : 'homeController';
@@ -32,8 +53,7 @@ class Application
             self::$prams = !empty($url) ? array_values($url) : [];
     }
 
-
-    // the method below allows adding parameter after controller/method
+    // the method below clears parameter from url, leaves only controller/method name
     static public function clearupParameter()
     {
         $request = trim($_SERVER['REQUEST_URI'], '/');
@@ -46,9 +66,15 @@ class Application
                 $url[1] = substr($url[1], 0, $position); 
                     }
                 }    
-                
-              return $url;   
+        // the code below allows to have parameter in bare case url, it clears url[0] of parameter
+        elseif (!empty($url[0])) {
+            if (strpos($url[0], '?') == 0) {
+                unset($url[0]); // unset url[0] 
+                }
             }
+            
+        return $url;   
+        }
            
     }
 
