ac8c330b781fc0f38fc7464cfec1476aae6f9831
Surf-house <reeeee@ukr.net>
2021-05-27 15:43:30 +0300

    Have covered tests CurrencyModel

--- public_html/tests/unit/CurrencyModelTest.php
+++ public_html/tests/unit/CurrencyModelTest.php
@@ -1,30 +1,25 @@
 <?php 
-// the code below is working well 
-// TO DO autoloading for all classes and models in currenct project 
-// require "/opt/lampp/htdocs/currency_converter.com/public_html/app/Models/UserModel.model.php";
 
-// class CurrencyModelTest extends PHPUnit\Framework\TestCase
-// {
-//     // public function test_call_currency_api()
-//     // {
-//     //     $model = new App\Models\Model;
-//     //     $object_result = $model->call_currency_api();
-//     //     $this->assertIsObject($object_result);
-//     // }
 
-//     public function  test_grab_currency_exchange_list()
-//     {
-//         // $currency_model_obj = new \App\Models\CurrencyModel;
-//         $currency_model_obj = new \App\Models\CurrencyModel;
-//         $array_result = $currency_model_obj->grab_currency_exchange_list();
-//         $this->assertIsArray($array_result);
-//     }
-//     // public function test_store_currency_exchanges_history()
-    // {
-    //     $model = new CurrencyModel;
-    //     $converter_object = new App\Converter();
-    //     $result_store_currency_exchanges_history = $model->store_currency_exchanges_history($converter_object);
-    // }
+class CurrencyModelTest extends PHPUnit\Framework\TestCase
+{
+
+    public function  test_grab_currency_exchange_list()
+    {
+        $currency_model_obj = new \App\Models\CurrencyModel;
+        $array_result = $currency_model_obj->grab_currencies_list();
+        $this->assertIsArray($array_result);
+    }
+
+    public function test_get_currency_rate_from_store()
+    {
+        $currency_model_obj = new \App\Models\CurrencyModel;
+        $array_result = $currency_model_obj->get_currency_rate_from_store();
+        $this->assertIsArray($array_result);    
+    }
+
+
+
 }
 
 ?> 
